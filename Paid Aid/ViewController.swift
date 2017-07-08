3574e00489512e00a826f565d6ba4e28ff970b75
jakeknowles <jakepknowles@gmail.com>
2017-07-08 09:50:15 -0700

    Getting GitHub working via Xcode

--- Paid Aid/ViewController.swift
+++ Paid Aid/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("I can't wait to push this project to Github")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
