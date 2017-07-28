905cc9799a6f3d11a3ef15152dbad38435813589
jakeknowles <jakepknowles@gmail.com>
2017-07-28 13:58:44 -0700

    Started UI for Login Screen - learning basics

--- Paid Aid/ViewController.swift
+++ Paid Aid/ViewController.swift
@@ -8,37 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
-    
-    //MARK: UITextFieldDelegate
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        //Hide the keyboard
-        textField.resignFirstResponder()
-        return true
-    }
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        paidAidNameLabel.text = textField.text
-    }
-    
-    
-    //MARK: Properties
-    @IBOutlet weak var paidAidTextField: UITextField!
-    @IBOutlet weak var paidAidNameLabel: UILabel!
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        paidAidTextField.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    //MARK: Actions
-    @IBAction func setPaidAidLabelText(_ sender: UIButton) {
-        paidAidNameLabel.text = "Default text"
+    
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to my first app", message:
+            "Hello Word", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,
+                                                handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
     }
+
 }
 
